1f8974964abb0c819b9ec4d4540476736dcc14d8
drgiga <drgiga1@gmail.com>
2022-06-24 17:12:21 +0430

    Create ListProducts  (from data.js & images )

--- amazona/frontend/src/App.js
+++ amazona/frontend/src/App.js
@@ -1,3 +1,5 @@
+import data from './data';
+
 function App() {
   return (
     <div>
@@ -5,7 +7,27 @@ function App() {
         <a href="/">BiLink Shop</a>
       </header>
 
-      <main>List Of Contents</main>
+      <main>
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a className="product-name" href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>${product.price}</strong>
+                </p>
+                <button>Add To Card</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
 
       <footer>Footer</footer>
     </div>
