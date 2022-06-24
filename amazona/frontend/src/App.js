404c47b15882bb521a749bc9c3eec352e332ccf3
drgiga <drgiga1@gmail.com>
2022-06-24 15:29:49 +0430

    Create-React-App

---
+++ amazona/frontend/src/App.js
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">BiLink Shop</a>
+      </header>
+
+      <main>List Of Contents</main>
+
+      <footer>Footer</footer>
+    </div>
+  );
+}
+
+export default App;
